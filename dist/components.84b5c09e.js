9cba9eaec1771e7c1900dedff5f846127d4fc025
Konstantin <mogilevkostya@yandex.ru>
2025-06-23 01:05:28 +0700

    Спойлеры: рефакторинг и реализация мульти-открытия

--- dist/components.84b5c09e.js
+++ dist/components.84b5c09e.js
@@ -1,24 +1,45 @@
-const spollers = document.querySelectorAll('.spoller');
-spollers.forEach((spoller)=>{
-    const spollerHeader = spoller.querySelector('.spoller__header');
-    const spollerContent = spoller.querySelector('.spoller__content');
-    spollerHeader.addEventListener('click', (event)=>{
-        event.preventDefault();
-        if (spoller.open) {
-            spoller.classList.remove('open');
+class Spollers {
+    constructor(wrapClass, multiOpen = false){
+        this.spollersWrap = document.querySelector(`.${wrapClass}`);
+        this.spollers = this.spollersWrap.querySelectorAll('.spoller');
+        this.activeSpollers = this.spollersWrap.querySelectorAll('.spoller[open]');
+        this.multiOpen = multiOpen == 'multiOpen' ? true : false;
+        this.events();
+        if (this.activeSpollers) this.activeSpollers.forEach((spoller)=>{
+            this.openSpoller(spoller);
+        });
+    }
+    events() {
+        this.spollers.forEach((spoller)=>{
+            spoller.addEventListener('click', (event)=>{
+                event.preventDefault();
+                const isHeaderClick = event.target.closest('.spoller__header');
+                if (!isHeaderClick) return;
+                if (spoller.open) this.closeSpoller(spoller);
+                else this.openSpoller(spoller);
+            });
+        });
+    }
+    openSpoller(spoller) {
+        const spollerContent = spoller.querySelector('.spoller__content');
+        spoller.setAttribute('open', '');
+        spollerContent.style.maxHeight = '0';
+        requestAnimationFrame(()=>{
             spollerContent.style.maxHeight = spollerContent.scrollHeight + 'px';
-            requestAnimationFrame(()=>{
-                spollerContent.style.maxHeight = '0';
-            });
-            setTimeout(()=>spoller.removeAttribute('open'), 300);
-        } else {
-            spoller.setAttribute('open', '');
+        });
+        if (this.activeSpoller && this.activeSpoller !== spoller && !this.multiOpen) this.closeSpoller(this.activeSpoller);
+        this.activeSpoller = spoller;
+    }
+    closeSpoller(spoller) {
+        const spollerContent = spoller.querySelector('.spoller__content');
+        spoller.classList.remove('open');
+        spollerContent.style.maxHeight = spollerContent.scrollHeight + 'px';
+        requestAnimationFrame(()=>{
             spollerContent.style.maxHeight = '0';
-            requestAnimationFrame(()=>{
-                spollerContent.style.maxHeight = spollerContent.scrollHeight + 'px';
-            });
-        }
-    });
-});
+        });
+        setTimeout(()=>spoller.removeAttribute('open'), 300);
+    }
+}
+new Spollers('spollers');
 
 //# sourceMappingURL=components.84b5c09e.js.map
